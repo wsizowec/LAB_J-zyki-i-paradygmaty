71ee454ec31e96ff9372919e2bde4d991015678b
WSIZOWEC <39623692+wsizowec@users.noreply.github.com>
2025-01-21 18:02:13 +0100

    Zad1

---
+++ lab6/zad1/Program.fs
@@ -0,0 +1,14 @@
+﻿//Zadanie 1. Liczba słów i znaków
+
+open System
+
+let countWordsAndChars (text: string) =
+    let words = text.Split([|' '|], StringSplitOptions.RemoveEmptyEntries)
+    let numWords = words.Length
+    let numChars = text.Replace(" ", "").Length
+    printfn "Liczba słów: %d" numWords
+    printfn "Liczba znaków (bez spacji): %d" numChars
+
+printfn "Wprowadź tekst:"
+let userInput = Console.ReadLine()
+countWordsAndChars userInput
