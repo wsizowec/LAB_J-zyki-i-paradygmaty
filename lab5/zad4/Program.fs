29ac59f5b4010a3bcde6502dc5c74f42d0269723
WSIZOWEC <39623692+wsizowec@users.noreply.github.com>
2025-01-21 17:54:19 +0100

    Zad4 lab5

---
+++ lab5/zad4/Program.fs
@@ -0,0 +1,37 @@
+﻿//Zadanie 4. Rekurencyjne rozwiązywanie problemu wież Hanoi
+
+
+printfn "============================="
+printfn "Część 1  (uzywanie rekurencji)"
+printfn "============================="
+
+let rec hanoi n source target auxiliary =
+    if n = 1 then
+        printfn "Move disk from %s to %s" source target
+    else
+
+        hanoi (n - 1) source auxiliary target
+        printfn "Move disk from %s to %s" source target
+        hanoi (n - 1) auxiliary target source
+
+hanoi 3 "A" "C" "B"
+printfn "============================="
+printfn "Część 2  (uzywanie iteracji)"
+printfn "============================="
+
+let hanoiIter n source target auxiliary =
+    let stack = [(n, source, target, auxiliary)]
+    let rec loop stack =
+        match stack with
+        | [] -> ()
+        | (0, _, _, _) :: rest -> loop rest 
+        | (n, source, target, auxiliary) :: rest ->
+            if n = 1 then
+                printfn "Move disk from %s to %s" source target
+                loop rest
+            else
+                loop ((n - 1, source, auxiliary, target) :: (n - 1, auxiliary, target, source) :: rest)
+    loop stack
+
+hanoiIter 3 "X" "Y" "Z"
+
