d89fa7d76e2aeb366c3693e50934b859237d9f90
WSIZOWEC <39623692+wsizowec@users.noreply.github.com>
2025-01-21 18:03:36 +0100

    Zad3-4

---
+++ lab6/zad3/Program.fs
@@ -0,0 +1,13 @@
+﻿//Zadanie 3: Usuwanie duplikatów
+open System
+
+let removeDuplicates (words: string list) =
+    words |> List.distinct
+
+printfn "Wprowadź słowa oddzielone spacjami:"
+let userInput = Console.ReadLine()
+let wordsList = userInput.Split(' ') |> List.ofArray
+let uniqueWords = removeDuplicates wordsList
+
+let result = String.Join(" ", uniqueWords)
+printfn "Unikalne słowa: %s" result
---
+++ lab6/zad4/Program.fs
@@ -0,0 +1,17 @@
+﻿//Zadanie 4: Zmiana formatu tekstu
+
+open System
+
+let formatText (text: string) =
+    let parts = text.Split(';') |> Array.map (fun s -> s.Trim())
+    if parts.Length = 3 then
+        let firstName = parts.[0]
+        let lastName = parts.[1]
+        let age = parts.[2]
+        printfn "%s, %s (%s lat)" lastName firstName age
+    else
+        printfn "Niepoprawny format danych."
+
+printfn "Wprowadź tekst w formacie 'imię; nazwisko; wiek':"
+let userInput = Console.ReadLine()
+formatText userInput
