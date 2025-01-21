06fb82ed84167d37344b9bba5cea3eb613654182
WSIZOWEC <39623692+wsizowec@users.noreply.github.com>
2025-01-21 18:01:10 +0100

    Zad5-6

---
+++ lab6/zad5/Program.fs
@@ -0,0 +1,15 @@
+﻿//Zadanie 5: Znajdowanie najdłuższego słowa
+
+open System
+
+let findLongestWords (text: string) =
+    let words = text.Split([|' '|], StringSplitOptions.RemoveEmptyEntries)
+    let maxLength = words |> Array.maxBy String.length |> String.length
+    let longestWords = words |> Array.filter (fun word -> word.Length = maxLength)
+
+    printfn "Najdłuższe słowa: %A" longestWords
+    printfn "Długość najdłuższych słów: %d" maxLength
+
+printfn "Wprowadź tekst:"
+let userInput = Console.ReadLine()
+findLongestWords userInput
---
+++ lab6/zad6/Program.fs
@@ -0,0 +1,21 @@
+﻿//Zadanie 6. Wyszukiwanie i zamiana
+
+open System
+
+let searchAndReplace (text: string) (oldWord: string) (newWord: string) =
+    if text.Contains(oldWord) then
+        let modifiedText = text.Replace(oldWord, newWord)
+        printfn "Zmodyfikowany tekst:\n%s" modifiedText
+    else
+        printfn "Nie znaleziono słowa '%s' w tekście." oldWord
+
+printfn "Wprowadź tekst:"
+let userInput = Console.ReadLine()
+
+printfn "Wprowadź słowo do wyszukania:"
+let wordToSearch = Console.ReadLine()
+
+printfn "Wprowadź nowe słowo:"
+let wordToReplace = Console.ReadLine()
+
+searchAndReplace userInput wordToSearch wordToReplace
