d0054ae57036213afcf81856cb479214ca59eb89
WSIZOWEC <39623692+wsizowec@users.noreply.github.com>
2025-01-21 12:49:22 +0100

    Zad3 lab5

---
+++ lab5/zad3/Program.fs
@@ -0,0 +1,20 @@
+﻿//Zadanie 3. Generowanie permutacji listy:
+
+let rec permutacje lst =
+    match lst with
+    | [] -> [[]]
+    | x :: xs ->
+        [for perm in permutacje xs do
+            for i in 0 .. List.length perm do
+                yield List.insertAt i x perm]
+
+[<EntryPoint>]
+let main argv =
+    let lista = [1; 2; 3]
+    let permutacjeLista = permutacje lista
+
+   
+    printfn "Permutacje listy %A:" lista
+    permutacjeLista |> List.iter (fun perm -> printfn "%A" perm)
+
+    0 
