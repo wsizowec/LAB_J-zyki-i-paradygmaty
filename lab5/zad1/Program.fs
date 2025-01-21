8fef3b1079f0d050b88784f33032ccb7974ab277
WSIZOWEC <39623692+wsizowec@users.noreply.github.com>
2025-01-21 12:37:39 +0100

    Zad1 lab5

---
+++ lab5/zad1/Program.fs
@@ -0,0 +1,16 @@
+﻿ //Zadanie 1
+    let rec fibRek n =
+        if n <= 0 then 0
+        elif n = 1 then 1
+        else fibRek (n - 1) + fibRek (n - 2)
+
+    let rec fibRekTail n a b =
+        if n = 0 then a
+        elif n = 1 then b
+        else fibRekTail (n - 1) b (a + b)
+
+    [<EntryPoint>]
+    let main argv = 
+        let wynik3 = fibRekTail 10 0 1
+        printf "Fib: %d\n" wynik3
+        0
