e9c058d8ed1ac5c3a2b90d6650a353d460051d76
WSIZOWEC <39623692+wsizowec@users.noreply.github.com>
2025-01-21 18:04:21 +0100

    Zadanie 2

---
+++ lab6/zad2/Program.fs
@@ -0,0 +1,15 @@
+﻿//Zadanie 2. Sprawdzenie palindromu
+
+open System
+
+let isPalindrome (text: string) =
+    let cleanedText = text.Replace(" ", "").ToLower()
+    let reversedText = new string(cleanedText.ToCharArray() |> Array.rev)
+    if cleanedText = reversedText then
+        printfn "To jest palindrom."
+    else
+        printfn "To nie jest palindrom."
+
+printfn "Wprowadź tekst:"
+let userInput = Console.ReadLine()
+isPalindrome userInput
